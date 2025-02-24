3ff73b38570af39e4e192d693664701f5af5a9e7
Kama-Pushka <ser123.ry@gmail.com>
2025-02-24 18:28:18 +0500

    inital

---
+++ src/main/java/org/javaspringcourse/JavaSpringCourseApplication.java
@@ -0,0 +1,13 @@
+package org.javaspringcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringCourseApplication.class, args);
+	}
+
+}
